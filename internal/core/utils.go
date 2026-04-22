7060d803ed2895ec19880cb7738bd7810c57fbbf
10xdev4u-alt <10xdev4u@gmail.com>
2026-04-23 03:43:54 +0530

    feat(core): implement random string and cookie synthesis

--- internal/core/utils.go
+++ internal/core/utils.go
@@ -23,3 +23,28 @@ func UnixTimestamp() int64 {
 }
 
 func Timestamp() int64 {
+	return time.Now().UnixNano() / int64(time.Millisecond)
+}
+
+func GenerateRandomString(length int, charset string) string {
+	var letters []rune
+	if charset == "numeric" {
+		letters = []rune("0123456789")
+	} else {
+		letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	}
+
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func GenerateCookie() string {
+	timestamp := UnixTimestamp()
+	items := []string{
+		fmt.Sprintf("Hm_lvt_358cae4815e85d48f7e8ab7f3680a74b=%d", timestamp-int64(rand.Intn(2592000))),
+		fmt.Sprintf("_ga=GA1.1.%s.%d", GenerateRandomString(10, "numeric"), timestamp-int64(rand.Intn(2592000))),
+		fmt.Sprintf("_ga_YXD8W70SZP=GS1.1.%d.1.1.%d.0.0.0", timestamp-int64(rand.Intn(2592000)), timestamp-int64(rand.Intn(2592000))),
+		fmt.Sprintf("Hm_lpvt_358cae4815e85d48f7e8ab7f3680a74b=%d", timestamp-int64(rand.Intn(2592000))),
