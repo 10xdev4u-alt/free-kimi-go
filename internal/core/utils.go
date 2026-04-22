c1b5e0d1caf1076beb6f677da405a025f9c6b396
10xdev4u-alt <10xdev4u@gmail.com>
2026-04-23 03:43:54 +0530

    feat(core): implement UUID and timestamp helpers

--- internal/core/utils.go
+++ internal/core/utils.go
@@ -1,1 +1,25 @@
-package core\nimport "github.com/google/uuid"
+package core
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func UUID(separator bool) string {
+	id := uuid.New().String()
+	if !separator {
+		return id
+	}
+	return id
+}
+
+func UnixTimestamp() int64 {
+	return time.Now().Unix()
+}
+
+func Timestamp() int64 {
